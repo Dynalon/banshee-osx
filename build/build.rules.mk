7918fba0e19a8d68708cbdf2c152f6a91960e399
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2011-08-17 12:45:37 +0200

    build: Remove GTK3 detection and conditional compilation
    We're not doing to do any conditional compilation for GTK3 stuff, the
    port is happening in the gtk3 branch which will be merged in when it's
    ready.

--- build/build.rules.mk
+++ build/build.rules.mk
@@ -22,10 +22,6 @@ if ENABLE_TESTS
     ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
 endif
 
-if HAVE_GTK3
-    GTK3_FLAG = "-define:GTK3"
-endif
-
 FILTERED_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE))
 DEP_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE) | sed s,-r:,,g | grep '$(top_builddir)/bin/')
 
@@ -64,7 +60,7 @@ $(ASSEMBLY_FILE): $(SOURCES_BUILD) $(RESOURCES_EXPANDED) $(DEP_LINK)
 		$(ASSEMBLY_BUILD_FLAGS) \
 		-nowarn:0278 -nowarn:0078 $$warn \
 		-debug -target:$(TARGET) -out:$@ \
-		$(BUILD_DEFINES) $(ENABLE_TESTS_FLAG) $(GTK3_FLAG) \
+		$(BUILD_DEFINES) $(ENABLE_TESTS_FLAG) \
 		$(FILTERED_LINK) $(RESOURCES_BUILD) $(SOURCES_BUILD)
 	@if [ -e $(srcdir)/$(notdir $@.config) ]; then \
 		cp $(srcdir)/$(notdir $@.config) $(top_builddir)/bin; \
