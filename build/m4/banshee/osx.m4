d71b1031124424fcfa226536c57a4575a635ad7c
Timo Dörr <timo@latecrew.de>
2012-05-15 21:07:38 +0200

    osx: Switch from ige-mac-integration to gtk-mac-integration (bgo#674576)
    Update DllImport references to new gtkmacintegration libs and update the
    ige_* symbols to gtk_* symbols.
    
    We're still using the old (deprecated) Carbon-based gtk-mac-integration
    API instead of the proposed GtkOSXApplication API, which resides in the
    same library.
    
    Also update Hyena to the latest git master, to get rid of unneeded
    references to libigemacintegration.
    
    Signed-off-by: Bertrand Lorentz <bertrand.lorentz@gmail.com>

--- build/m4/banshee/osx.m4
+++ build/m4/banshee/osx.m4
@@ -3,7 +3,7 @@ AC_DEFUN([BANSHEE_CHECK_OSX],
 	enable_osx="no"
 	if test "x${host_os%${host_os#??????}}" = "xdarwin"; then
 		enable_osx="yes"
-		PKG_CHECK_MODULES(IGEMACINTEGRATION, ige-mac-integration >= 0.8.6)
+		PKG_CHECK_MODULES(GTKMACINTEGRATION, gtk-mac-integration >= 1.0.1)
 		PKG_CHECK_MODULES(MONOMAC, monomac >= 0.7)
 		MONOMAC_ASSEMBLIES=`$PKG_CONFIG --variable=Libraries monomac`
 		AC_SUBST(MONOMAC_LIBS)
--- src/Backends/Banshee.Osx/OsxIntegration.Ige/IgeMacMenu.cs
+++ src/Backends/Banshee.Osx/OsxIntegration.Ige/IgeMacMenu.cs
@@ -31,41 +31,41 @@ namespace OsxIntegration.Ige
 {
     public static class IgeMacMenu
     {
-        [DllImport ("libigemacintegration.dylib")]
-        private static extern void ige_mac_menu_connect_window_key_handler (IntPtr window);
+        [DllImport ("libgtkmacintegration.dylib")]
+        private static extern void gtk_mac_menu_connect_window_key_handler (IntPtr window);
 
         public static void ConnectWindowKeyHandler (Gtk.Window window)
         {
-            ige_mac_menu_connect_window_key_handler (window.Handle);
+            gtk_mac_menu_connect_window_key_handler (window.Handle);
         }
 
-        [DllImport ("libigemacintegration.dylib")]
-        private static extern void ige_mac_menu_set_global_key_handler_enabled (bool enabled);
+        [DllImport ("libgtkmacintegration.dylib")]
+        private static extern void gtk_mac_menu_set_global_key_handler_enabled (bool enabled);
 
         public static bool GlobalKeyHandlerEnabled {
-            set { ige_mac_menu_set_global_key_handler_enabled (value); }
+            set { gtk_mac_menu_set_global_key_handler_enabled (value); }
         }
 
-        [DllImport ("libigemacintegration.dylib")]
-        static extern void ige_mac_menu_set_menu_bar (IntPtr menu_shell);
+        [DllImport ("libgtkmacintegration.dylib")]
+        static extern void gtk_mac_menu_set_menu_bar (IntPtr menu_shell);
 
         public static Gtk.MenuShell MenuBar {
-            set { ige_mac_menu_set_menu_bar (value == null ? IntPtr.Zero : value.Handle); }
+            set { gtk_mac_menu_set_menu_bar (value == null ? IntPtr.Zero : value.Handle); }
         }
 
-        [DllImport ("libigemacintegration.dylib")]
-        private static extern void ige_mac_menu_set_quit_menu_item (IntPtr quit_item);
+        [DllImport ("libgtkmacintegration.dylib")]
+        private static extern void gtk_mac_menu_set_quit_menu_item (IntPtr quit_item);
 
         public static Gtk.MenuItem QuitMenuItem {
-            set { ige_mac_menu_set_quit_menu_item (value == null ? IntPtr.Zero : value.Handle); }
+            set { gtk_mac_menu_set_quit_menu_item (value == null ? IntPtr.Zero : value.Handle); }
         }
 
-        [DllImport ("libigemacintegration.dylib")]
-        private static extern IntPtr ige_mac_menu_add_app_menu_group ();
+        [DllImport ("libgtkmacintegration.dylib")]
+        private static extern IntPtr gtk_mac_menu_add_app_menu_group ();
 
         public static IgeMacMenuGroup AddAppMenuGroup ()
         {
-            var native = ige_mac_menu_add_app_menu_group ();
+            var native = gtk_mac_menu_add_app_menu_group ();
             return native == IntPtr.Zero
                 ? null
                 : (IgeMacMenuGroup)GLib.Opaque.GetOpaque (native, typeof (IgeMacMenuGroup), false);
