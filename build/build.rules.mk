Dynalon/banshee-osx

commit 3e6d7a1750f1bec55ec1e81f25cce7b91eb2372e

build: cosmetic tabs vs spaces correction
Makefiles are actually the only exception to the 4-spaces rule in the
coding guidelines, so we mention this from now on in HACKING.

build/build.rules.mk
@@ -8,8 +8,8 @@ INSTALL_ICONS = $(top_srcdir)/build/private-icon-theme-installer "$(mkinstalldir
 INSTALL_DIR_RESOLVED = $(firstword $(subst , $(DEFAULT_INSTALL_DIR), $(INSTALL_DIR)))
 
 if ENABLE_TESTS
-    LINK += " $(NUNIT_LIBS)"
-    ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
+	LINK += " $(NUNIT_LIBS)"
+	ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
 endif
 
 FILTERED_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE))

HACKING
@@ -80,7 +80,7 @@ guidelines. If code is in violation, you will be asked to reformat it.
 
       Dictionary<K, V> not Dictionary<K,V>
 
-  9. Use 4 space characters for indentation, NOT tabs
+  9. Use 4 space characters for indentation, NOT tabs (except in Makefiles)
    
   10. Try to observe a 120 character wrap margin. If your lines are over 120
      characters, break and indent them logically.

commit 15d6fddd1e10b93fef95ddea118701c7c6953c63

build: partially revert previous commit to fix the build
Apologies, 3e6d7a1750f1bec55ec1e81f25cce7b91eb2372e broke the build [1]
so we need to restore what seems to be the only 4spaces occurrence in
the makefiles. Don't ask me why it doesn't work with a tabs...

[1] Making all in Banshee.AmazonMp3
  GEN   bamz
/bin/bash: LINK: command not found
make[4]: *** [bamz] Error 127
make[3]: *** [all-recursive] Error 1
make[2]: *** [all-recursive] Error 1
make[1]: *** [all-recursive] Error 1
make: *** [all] Error 2

build/build.rules.mk
@@ -8,8 +8,8 @@ INSTALL_ICONS = $(top_srcdir)/build/private-icon-theme-installer "$(mkinstalldir
 INSTALL_DIR_RESOLVED = $(firstword $(subst , $(DEFAULT_INSTALL_DIR), $(INSTALL_DIR)))
 
 if ENABLE_TESTS
-	LINK += " $(NUNIT_LIBS)"
-	ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
+    LINK += " $(NUNIT_LIBS)"
+    ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
 endif
 
 FILTERED_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE))